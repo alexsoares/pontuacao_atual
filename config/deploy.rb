971ad5bc4ae72b60aa119c99d90c939fbb0360b7
Manutencao <alexandrejr_soares@yahoo.com.br>
2012-07-13 10:52:15 -0300

    exclusao 503.html

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,33 @@
-set :application, "test.pontuacao.seducpma.com"
-set :repository,  "git://github.com/alexsoares/pontuacao_atual.git"
+#set :application, "187.35.111.69"
+set :application, "192.168.0.122"
+set :repository, "git://github.com/alexsoares/pontuacao_atual.git"
 set :user, "servidor"
 set :use_sudo, false
-set :deploy_to, "/home/#{user}/#{application}"
+set :deploy_to, "/home/#{user}/pontuacao.seducpma.com"
+
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-server application, :app, :web, :db, :primary => true
 
-# If you are using Passenger mod_rails uncomment this:
+server application, :app, :web, :db, :primary => true
+after "deploy:update_code", "deploy:custom_symlinks"
  namespace :deploy do
    task :start do ; end
    task :stop do ; end
    task :restart, :roles => :app, :except => { :no_release => true } do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
+
+   task :custom_symlinks do
+     run "rm -rf #{release_path}/config/database.yml"
+     run "ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
+     run "ln -s #{shared_path}/503.html #{release_path}/public/503.html"
+     run "cd #{deploy_to}/current && /usr/bin/env rake db:migrate RAILS_ENV=production"
+   end
+
+    desc "Update the crontab file"
+    task :update_crontab, :roles => :db do
+      run "cd #{release_path} && whenever --update-crontab #{application}"
+    end
+
+
  end
